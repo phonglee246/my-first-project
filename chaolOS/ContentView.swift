d1004e0048e95fc60e2c7708916187f86aee4715
phonglee246 <124367757+phonglee246@users.noreply.github.com>
2023-02-08 10:09:43 +0700

    ghi chu dau tien

---
+++ chaolOS/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  chaolOS
+//
+//  Created by phong on 16/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chaolOS/chaolOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  chaolOSApp.swift
+//  chaolOS
+//
+//  Created by phong on 16/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct chaolOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
